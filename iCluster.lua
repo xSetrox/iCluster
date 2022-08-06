87452b9b64df0a11ca8bc5d9882a64334de270a2
Lance <lance.faltinsky@gmail.com>
2022-08-06 14:54:57 -0400

    kph support, fixed gauges displaying when not in a car

--- iCluster.lua
+++ iCluster.lua
@@ -10,6 +10,12 @@ low_beam = directx.create_texture(resources_dir .. 'lowbeam.png')
 tpms = directx.create_texture(resources_dir .. 'tpms.png')
 traction_control = directx.create_texture(resources_dir .. 'traction.png')
 
+unit = 1
+menu.list_select(menu.my_root(), "Units", {"iclusterunits"}, "", {"MPH", "KPH"}, 1, function(index)
+    unit = index 
+end)
+
+
 white = {
     r = 1,
     g = 1,
@@ -40,8 +46,8 @@ green = {
 
 local lights, high_lights = memory.alloc_int(), memory.alloc_int()
 while true do 
-    vehicle =  entities.get_user_vehicle_as_handle()
-    if vehicle ~= 0 then 
+    vehicle = PED.GET_VEHICLE_PED_IS_IN(players.user_ped(), false)
+    if vehicle ~= 0 and PED.IS_PED_IN_ANY_VEHICLE(players.user_ped(), true) then 
         local vecs = ENTITY.GET_ENTITY_SPEED_VECTOR(vehicle, true)
         local v_hdl = entities.handle_to_pointer(vehicle)
         local speed = ENTITY.GET_ENTITY_SPEED(vehicle)
@@ -49,7 +55,16 @@ while true do
         local kph = speed * 3.6
         local max = VEHICLE.GET_VEHICLE_ESTIMATED_MAX_SPEED(vehicle)
         local max_mph = max * 2.236936
-        local speed_rotation = (mph/max_mph)*0.32
+        local max_kph = max * 3.6
+
+        if unit == 1 then 
+            measured_speed = mph 
+            measured_max = max_mph
+        else
+            measured_speed = kph 
+            measured_max = max_kph
+        end
+        local speed_rotation = (measured_speed/measured_max)*0.32
         local rpm = entities.get_rpm(v_hdl)
         if rpm == 1 then 
             -- rev limiter simulation
@@ -59,7 +74,7 @@ while true do
         directx.draw_texture(speedometer_case, 0.05, 0.05, 0.5, 0.5, 0.7, 0.8, 0, white)
         directx.draw_texture(needle, 0.023, 0.023, 0.88, 0.125, 0.70, 0.82, speed_rotation, white)
         -- speed text also i guess what
-        directx.draw_text(0.699, 0.87, math.ceil(mph), 5, 0.8, white, true)
+        directx.draw_text(0.699, 0.87, math.ceil(measured_speed), 5, 0.8, white, true)
         -- rpm gauge
         directx.draw_texture(tach_case, 0.05, 0.05, 0.5, 0.5, 0.83, 0.8, 0, white)
         -- rpm needle
